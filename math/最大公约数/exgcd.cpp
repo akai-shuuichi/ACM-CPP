a67ba6f7e0b3b4f04ea66d5474d244384da33423
gzx <928926035@qq.com>
2020-01-19 21:12:01 +0800

    first commit

--- math/最大公约数/exgcd.cpp
+++ math/最大公约数/exgcd.cpp
@@ -1,8 +1,10 @@
 //三个愿望 一次满足
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int Exgcd(int a, int b, int &x, int &y) {//计算ax+by=gcd(a,b)的特解
-  if (!b) {
+int Exgcd(int a, int b, int &x, int &y)
+{ //计算ax+by=gcd(a,b)的特解
+  if (!b)
+  {
     x = 1;
     y = 0;
     return a;
@@ -13,10 +15,12 @@ int Exgcd(int a, int b, int &x, int &y) {//计算ax+by=gcd(a,b)的特解
   y = t - (a / b) * y;
   return d;
 }
-int main(){
-int a,b,x,y;
-cin>>a>>b;
-//cout<<__gcd(a,b);
-cout<<Exgcd(a,b,x,y)<<endl;;
-cout<<x<<" "<<y;
+int main()
+{
+  int a, b, x, y;
+  cin >> a >> b;
+  //cout<<__gcd(a,b);
+  cout << Exgcd(a, b, x, y) << endl;
+  ;
+  cout << x << " " << y;
 }
