7453cc297b1754a44d4683ac1eaad34af8ff6cf7
gzx <928926035@qq.com>
2020-01-19 19:02:34 +0800

    first commit

---
+++ math/素数/单个素数.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int a) {
+  if (a < 2) return 0;
+  for (int i = 2; i * i <= a; ++i)
+    if (a % i) return false;
+  return true;
+}
+int main(){
+int a;
+cin>>a;
+cout<<isPrime(a);
+
+}
